99888dc1948f8f6ede0e8851ae9d8a387576a157
Marty Alcala <marty@bitpay.com>
2021-08-05 17:21:08 -0400

    link to gist

--- packages/bitcore-lib-cash/lib/script/escrow.js
+++ packages/bitcore-lib-cash/lib/script/escrow.js
@@ -4,6 +4,9 @@ const Hash = require('../crypto/hash');
 const Opcode = require('../opcode');
 const PublicKey = require('../publickey');
 
+// This file implements the Zero-Conf Forfeit scripts defined here: 
+// https://ide.bitauth.com/import-gist/104baff7503d6a7ad619ad814153b059
+
 const Escrow = {};
 
 const pushOpcodeOrBufferFromNumber = function(n) {
